96fe189645a15c5a364c6f29653e603817f78fc8
Millzy <millzyyt08@gmail.com>
2023-04-03 16:23:33 +0100

    patch

---
+++ MillzysMod.ModMenu/Mod.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using MelonLoader;
+using BuildInfo = MillzysMod.ModMenu.Properties.BuildInfo;
+
+namespace MillzysMod.ModMenu
+{
+    public class Mod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HarmonyInstance.PatchAll();
+            MelonLogger.Msg($"Finished initialising {nameof(ModMenu)}.");
+        }
+    }
+}
