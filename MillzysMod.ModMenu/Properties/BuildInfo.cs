1a59b0e85f768c518f3bee4c7d009a601cbf8211
Millzy <millzyyt08@gmail.com>
2023-04-03 15:22:40 +0100

    add assembly info

---
+++ MillzysMod.ModMenu/Properties/BuildInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MillzysMod.ModMenu.Properties
+{
+    internal static class BuildInfo
+    {
+        public const string ID = "MillzysMod.ModMenu";
+        public const string Description = "Adds an additional menu that displays currently installed mods (and any errors) in the main menu.";
+        public const string Name = "Millzy's ModMenu";
+        public const string Author = "Millzy";
+        public const string Version = "0.1.0";
+    }
+}
