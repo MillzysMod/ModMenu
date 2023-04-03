c5e0c7756b288445806e093cd2ba522da3a9f347
Millzy <millzyyt08@gmail.com>
2023-04-03 16:23:43 +0100

    add the modifer behaviour

---
+++ MillzysMod.ModMenu/HarmonyPatches/GameControl_startupPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using MillzysMod.ModMenu.CustomTypes;
+using SLZ.Bonelab;
+
+namespace MillzysMod.ModMenu.HarmonyPatches
+{
+    [HarmonyPatch(typeof(GameControl_startup), "Start")]
+    public static class GameControl_startupPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix() => MainMenuModifier.Begin();
+    }
+}
