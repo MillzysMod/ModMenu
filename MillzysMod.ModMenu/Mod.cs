86c2968047e8d320ef1dfd054e0dd460db7a2b7d
Millzy <millzyyt08@gmail.com>
2023-04-04 07:56:13 +0100

    comment

--- MillzysMod.ModMenu/Mod.cs
+++ MillzysMod.ModMenu/Mod.cs
@@ -4,4 +4,5 @@ namespace MillzysMod.Modmenu;
 
 public class Mod : MelonMod
 {
+    // Dont even really need anything here because MelonLoader handles most of the stuff for me. Maybe if I decide to do Boneject.
 }
